aeaa4e1d47f69dd3be129993487070e366cad023
visheshgupta0404 <visheshg990@gmail.com>
2026-01-06 16:09:04 +0530

    Adding CSS

--- script.js
+++ script.js
@@ -18,11 +18,16 @@ function checkWinner() {
     (array[3] !== null && array[3] === array[4] && array[3] === array[5]) ||
     (array[6] !== null && array[6] === array[7] && array[6] === array[8])
   ) {
-    result.style.color = "red";  
-    result.innerText = `Winner is ${currentPlayer}`;
+    result.style.color = "red";
+    result.innerHTML = `<h3>Winner is ${currentPlayer}<br><br>Game restart in 3 sec</h3>`;
+    setTimeout(()=>{
+      window.location.reload()
+    },3*1000)
+    
   } else if (!array.includes(null)) {
     result.style.color = "red";
     result.innerText = "Draw Press restart";
+    
   }
 }
 
@@ -32,7 +37,13 @@ function handleClick(el) {
   currentPlayer === "X" ? (currentPlayer = "O") : (currentPlayer = "X");
   array[el.id] = currentPlayer;
   console.log(array);
-  el.innerText = currentPlayer;
+  if (currentPlayer === "X") {
+    el.style.color = "#1F3A8A";
+    el.innerText = currentPlayer;
+  } else {
+    el.style.color = "#C2410C";
+    el.innerText = currentPlayer;
+  }
   checkWinner();
 }
 
